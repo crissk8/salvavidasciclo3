8b8463934d51c3f60f1ecca048591bdec546ded3
crissk8 <tarapuezcristian5@gmail.com>
2022-10-06 10:23:44 -0500

    documentacion salvavidas 1.0

---
+++ CRUD - MVC/demo_sb_maven/src/test/java/com/jtl/demo_sb_maven/DemoSbMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtl.demo_sb_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSbMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
