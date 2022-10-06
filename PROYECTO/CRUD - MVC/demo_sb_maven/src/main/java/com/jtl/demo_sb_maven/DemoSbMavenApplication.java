cde822b8b245baf98c55b31a44cf9196cd9d0957
crissk8 <tarapuezcristian5@gmail.com>
2022-10-06 11:04:37 -0500

    documentacion salvavidas 2.0

---
+++ PROYECTO/CRUD - MVC/demo_sb_maven/src/main/java/com/jtl/demo_sb_maven/DemoSbMavenApplication.java
@@ -0,0 +1,13 @@
+package com.jtl.demo_sb_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSbMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSbMavenApplication.class, args);
+	}
+
+}
---
+++ PROYECTO/CRUD - MVC/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO students(id, email, first_name, last_name) values(1,'pepitortiz@gmail.com','Pepito','Ortiz');
+INSERT INTO students(id, email, first_name, last_name) values(2,'andressolarte@gmail.com','Andres', 'Solarte');
+INSERT INTO students(id, email, first_name, last_name) values(3,'miguelperez@gmail.com','Miguel', 'Perez');
+INSERT INTO students(id, email, first_name, last_name) values(4,'lauranavia@gmail.com','Laura', 'Navia');
+
+INSERT INTO Usuario(id, usuario, clave) values(1,'admin','admin');
+INSERT INTO Usuario(id, usuario, clave) values(3,'profesor','1234');
+
+INSERT INTO courses(id,credits,fee,modules,name) values (1,3,4.5,4.3,'POO')
+INSERT INTO courses(id,credits,fee,modules,name) values (2,3,3.5,4.0,'Bases de datos I')
+INSERT INTO courses(id,credits,fee,modules,name) values (3,2,3.5,3.5,'Proyecto2')
